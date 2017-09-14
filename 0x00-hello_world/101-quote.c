312f4d4f77408b2d0c3c0b2951cccab96c1619ad
notjamesheck <224@holbertonschool.com>
2017-09-14 09:19:43 +0000

    Exercise 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, strlen(str));
+
+	return (1);
+}
