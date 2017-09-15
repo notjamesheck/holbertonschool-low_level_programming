3ad34e82f9600a2b4ca2c30983ddb26b87928050
notjamesheck <224@holbertonschool.com>
2017-09-15 22:25:01 +0000

    Exercise 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 99; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (b != 99 || b - a != 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
