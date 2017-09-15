85ed4502cc417e250ea4966efffb5a5d06091eb3
notjamesheck <224@holbertonschool.com>
2017-09-15 07:09:06 +0000

    Placate checker

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int a = '0';
-
-	int b = '1';
+	int a = '0', b = '1';
 
 	while (a < '9')
 	{
 		while (b <= '9')
 		{
-				putchar(a);
-				putchar(b);
-				putchar(',');
-				putchar(' ');
-				b++;
+			putchar(a);
+			putchar(b);
+
+			if (a < '8' || b < '9')
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			b++;
 		}
-
 		a++;
-		b++;
+		b = a + 1;
 	}
-
+	putchar('\n');
 	return (0);
 }
