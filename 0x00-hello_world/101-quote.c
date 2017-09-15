d482524262960a9323e994198f58660b5ae5207d
notjamesheck <224@holbertonschool.com>
2017-09-15 23:55:52 +0000

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
 */
 int main(void)
 {
-	char korpar[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, korpar, strlen(korpar));
+	write(2, str, strlen(str));
 
 	return (1);
 }
