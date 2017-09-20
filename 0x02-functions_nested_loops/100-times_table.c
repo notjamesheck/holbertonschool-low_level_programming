04ccb475ba491f841d01613769c21b8e3f298dd1
notjamesheck <224@holbertonschool.com>
2017-09-20 04:00:41 +0000

    Exercise 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+/**
+* print_times_table - check the code for Holberton School students.
+* @n: int parameter
+* Return: Always 0.
+*/
+void print_times_table(int n)
+{
+	int c, m;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	for (c = 0; c <= n; c++)
+	{
+		for (m = 0; m <= n; m++)
+		{
+			if (((m * c) > 99))
+			{
+				_putchar(((m * c) / 100) + '0');
+				_putchar((((m * c) % 100) / 10) + '0');
+				_putchar(((m * c) % 10) + '0');
+
+
+			}
+			else if ((m * c) > 9)
+			{
+
+				_putchar(' ');
+				_putchar(((m * c) / 10) + '0');
+				_putchar(((m * c) % 10) + '0');
+			}
+			else if (m == 0)
+			{
+				_putchar((m * c) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((m * c) + '0');
+			}
+			if (m != n)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+
+}
