326c5a02cd348b56bb075db3b00cbe3aa3e75952
notjamesheck <224@holbertonschool.com>
2017-09-26 17:00:04 +0000

    7-puts

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,20 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
-* main - check the code for Holberton School students.
-*
+* puts_half - check the code for Holberton School students.
+* @str: char string pointer
 * Return: Always 0.
 */
 void puts_half(char *str)
 {
-	int i = 0, m = 0;
+	int i = 0, c = 0;
 
 	for (i = 0; *(str + i) != '\0'; i++)
-	printf("%d\n", i);
-	m = (i - 1) / 2;
-	printf("%d\n", m);
+	{}
+	while (c <= ((i - 1) / 2))
+	{
+		_putchar(*str + (c + (i / 2)));
+		c++;
+	}
 	_putchar('\n');
 }
