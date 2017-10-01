48c87561c04135ca0441f24dd5bf6fdb39ad9a4f
notjamesheck <224@holbertonschool.com>
2017-10-01 06:45:34 +0000

    cleanup

--- 0x03-more_functions_nested_loops/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	long int num =  612852475143;
-	long int div = 2, maxFact;
-
-	while () 
-}
--- 0x04-pointers_arrays_strings/null.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	char str[0];
-
-	printf("%c\n", str[0]);
-	return (0);
-}
