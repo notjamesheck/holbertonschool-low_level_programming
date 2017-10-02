7bc865e247815d288a44a0e45d798e24556ed80b
notjamesheck <224@holbertonschool.com>
2017-10-02 23:55:52 +0000

    ojpef

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,17 @@
 */
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	while (*s != '\0')
 	{
 		if (*s == c)
 		{
-			break;
+			return (s);
 		}
-		s++;
+		++s;	
 	}
-	return (s);
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
 }
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -97,4 +97,6 @@ char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
 
+#define NULL ((char *)0)
+
 #endif /* HOLBERTON_H */
