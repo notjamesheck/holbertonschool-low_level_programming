dd2f81434b3bd8cccaf7538cc4e247a57f74be00
notjamesheck <224@holbertonschool.com>
2017-10-03 22:40:31 +0000

    exercise 8

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* set_string - Entry Point
+* @s: double pointer to char array
+* @to: pointer to char
+* Return: pointer to char
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
