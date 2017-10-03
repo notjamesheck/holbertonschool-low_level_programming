350cc8d3eef5876bf635496cd7a396fb9e124b00
notjamesheck <224@holbertonschool.com>
2017-10-03 04:57:01 +0000

    Exercise 6

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_chessboard - Entry point
+* @a: pointer to a char array
+* Return: print alphanumeric characters
+*/
+void print_chessboard(char (*a)[8])
+{
+	int x = 0, y;
+
+	while (x <= 8)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+			if (y == 7 && x != 8)
+			{
+				_putchar('\n');
+			}
+		}
+		x++;
+	}
+}
