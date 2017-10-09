fb398787c37b3149692f5921e451c619acd38bd9
notjamesheck <224@holbertonschool.com>
2017-10-09 06:03:56 +0000

    again

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -15,11 +15,6 @@ int main(int argc, char *argv[])
 
 	if (argc <= 2)
 	{
-		if (argc == 1)
-		{
-			printf("Error\n");
-			return (1);
-		}
 		printf("0\n");
 		return (1);
 	}
@@ -39,4 +34,3 @@ int main(int argc, char *argv[])
 	printf("%d\n", sum);
 	return (0);
 }
-/* comment */
