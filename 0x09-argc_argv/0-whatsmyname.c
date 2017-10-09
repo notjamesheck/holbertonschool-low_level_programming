e40cea84ae8837f208e2660507300e4767dc91a7
notjamesheck <224@holbertonschool.com>
2017-10-09 02:49:59 +0000

    checker

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* @argc: argument count
+* @argv: argument vector
+* Return: void
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
