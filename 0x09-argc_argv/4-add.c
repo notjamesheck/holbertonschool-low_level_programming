fe92965f49b625ba6a78455e042ff31daf4bf027
notjamesheck <224@holbertonschool.com>
2017-10-09 05:58:46 +0000

    4

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* @argc: argument count
+* @argv: argument vector
+* Return: void
+*/
+int main(int argc, char *argv[])
+{
+	int count = 1;
+	int sum = 0;
+	int sec = 0;
+
+	if (argc <= 2)
+	{
+		if (argc == 1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		printf("0\n");
+		return (1);
+	}
+
+	for (count = 1; count < argc; count++)
+	{
+		for (sec = 0; argv[count][sec] != '\0'; sec++)
+		{
+			if (!(argv[count][sec] >= '0' && argv[count][sec] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[count]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+/* comment */
