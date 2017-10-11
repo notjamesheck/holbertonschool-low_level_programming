f3f0d9f2d99c00a154ae9485203d926ba658809d
notjamesheck <224@holbertonschool.com>
2017-10-11 03:18:27 +0000

    o

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -17,12 +17,23 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	buff = malloc(sizeof(unsigned int) * size);
+	if (c == 0)
+	{
+		return (0);
+	}
+
+	buff = (char *)malloc(sizeof(unsigned int) * size);
 
 	while (i < size)
 	{
 		buff[i] = c;
 		i++;
 	}
+
+	if (buff == NULL)
+	{
+		return (NULL);
+	}
+
 	return (buff);
 }
