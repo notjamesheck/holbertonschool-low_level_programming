54ee43f73ee1a05221b372dae12d16d43491b3dc
notjamesheck <224@holbertonschool.com>
2017-10-11 02:26:57 +0000

    malloc exercise 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -10,7 +10,14 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-	char *buff = malloc(sizeof(unsigned int) * size);
+	char *buff;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	buff = malloc(sizeof(unsigned int) * size);
 
 	while (i < size)
 	{
