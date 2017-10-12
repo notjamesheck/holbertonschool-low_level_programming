96188c46acbf43c5f8452cd2c27d3986a883289a
notjamesheck <224@holbertonschool.com>
2017-10-12 02:50:27 +0000

    Task 4

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -132,6 +132,8 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 
 int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
 /* #define NULL ((char *)0) */
 
 #endif /* HOLBERTON_H */
