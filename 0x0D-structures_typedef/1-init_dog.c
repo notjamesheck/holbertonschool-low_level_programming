98d6f95096cd988407b3cf34c54f206615b8f2bc
notjamesheck <224@holbertonschool.com>
2017-10-16 23:54:50 +0000

    1

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+* init_dog - Entry point
+* @d: struct pointer
+* @name: string pointer
+* @age: float parameter
+* @owner: string pointer
+* Return: void
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
