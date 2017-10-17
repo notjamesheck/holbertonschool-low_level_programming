1c309fb1a069416c0a6aaacab080ad87450c773b
notjamesheck <224@holbertonschool.com>
2017-10-17 00:16:36 +0000

    2

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -20,11 +20,6 @@ void print_dog(struct dog *d)
 		else
 		printf("Name: %s\n", d->name);
 
-		if (d->age == 0)
-		{
-			printf("Age: (nil)\n");
-		}
-		else
 		printf("Age: %.6f\n", d->age);
 
 		if (d->owner == 0)
