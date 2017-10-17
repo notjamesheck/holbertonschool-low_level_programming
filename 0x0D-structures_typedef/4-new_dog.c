ae958c0ae01227713145339aca8069e93f3701a7
notjamesheck <224@holbertonschool.com>
2017-10-17 00:20:29 +0000

    4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,10 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* new_dog - Entry point
+* @name: string pointer
+* @age: float parameter
+* @owner: string pointer
+* Return: struct pointer
+*/
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+
 	new = malloc(sizeof(dog_t));
+
 	if (new == NULL)
 		return (NULL);
 	new->name = name;
