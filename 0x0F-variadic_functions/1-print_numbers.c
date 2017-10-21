57ec523d602b4e81b0c219f72c0c481d713dd7cb
notjamesheck <224@holbertonschool.com>
2017-10-21 19:59:45 +0000

    revise 1

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -10,18 +10,18 @@
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 1;
+	unsigned int i = 0;
 	va_list ap;
 
 	va_start(ap, n);
 
-	while (i <= n)
+	while (i < n)
 	{
 		printf("%d", va_arg(ap, int));
 
-		if (i != n && separator == NULL)
+		if (i + 1 != n && separator == NULL)
 		{}
-		else if (i != n && separator != NULL)
+		else if (i + 1 != n && separator != NULL)
 			printf("%s", separator);
 		else
 			printf("\n");
