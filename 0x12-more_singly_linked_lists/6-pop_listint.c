28ebbce0aa2ffd09712dcca5aa7ca17be6e7be1d
notjamesheck <224@holbertonschool.com>
2017-10-30 19:51:46 +0000

    Exercise 6

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* pop_listint - Entry point
+* @head: pointer to node
+* Return: head node data
+*/
+int pop_listint(listint_t **head)
+{
+	int val = 0;
+	listint_t *temp = *head;
+
+	if (head == NULL)
+	{
+		return (0); }
+	val = (*head)->n;
+	*head = (*head)->next;
+	free(temp);
+	return (val);
+}
