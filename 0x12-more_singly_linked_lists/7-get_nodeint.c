a2b8afa04f4a750e668ad10f42c5080a98ed4f53
notjamesheck <224@holbertonschool.com>
2017-10-30 20:21:40 +0000

    Exercise 8

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* get_nodeint_at_index - Entry point
+* @head: node
+* @index: int parameter
+* Return: node at index
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (i < index)
+	{
+		head = head->next;
+		i++; }
+	if (head == NULL)
+	{
+		return (NULL); }
+	return (head);
+}
---
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* sum_listint - Entry point
+* @head: node
+* Return: sum of node data
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0); }
+	while (head->next != NULL)
+	{
+		sum += head->n;
+		head = head->next; }
+	sum += head->n;
+	return (sum);
+}
