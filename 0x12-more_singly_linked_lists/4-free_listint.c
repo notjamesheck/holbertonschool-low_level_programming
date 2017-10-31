641a0978ac954ce2f3349e2386b7951e24d38466
James Heck <james.heck1@gmail.com>
2017-10-31 16:18:46 +0000

    4

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* free_listint - Entry point
+* @head: pointer to node
+* Return: void
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head->next != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp); }
+	temp = head;
+	free(temp);
+	head = NULL;
+}
