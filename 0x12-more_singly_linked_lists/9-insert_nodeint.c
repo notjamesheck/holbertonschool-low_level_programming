1cdee9763b6fcdb94c51e1b3d4cb85fe68a620f5
James Heck <james.heck1@gmail.com>
2017-11-01 02:55:12 +0000

    9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
 * insert_nodeint_at_index - Entry point
 * @head: pointer to node
@@ -10,23 +9,23 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 1;
-	listint_t *temp = *head;
 	listint_t *new = (listint_t *) malloc(sizeof(listint_t));
+	listint_t *temp = *head;
 
-	if (new == NULL)
-	{
-		return (NULL); }
 	if (*head == NULL)
 	{
 		return (NULL); }
-	new->n = n;
 	while (i < idx)
 	{
 		temp = temp->next;
+		if (temp == NULL)
+		{
+			return (NULL); }
 		i++; }
 	if (temp == NULL)
 	{
 		return (NULL); }
+	new->n = n;
 	new->next = temp->next;
 	temp->next = new;
 	return (new);
