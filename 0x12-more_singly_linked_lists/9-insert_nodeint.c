f3acfcaa5c6d2826a2ac64b3ebba13fc903c703c
James Heck <james.heck1@gmail.com>
2017-11-01 04:12:34 +0000

    9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
+	unsigned int i = 0;
 	listint_t *new = (listint_t *) malloc(sizeof(listint_t));
 	listint_t *temp = *head;
 
