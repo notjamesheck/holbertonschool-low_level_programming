3f333e4cd77e133867b286b8666767754d2746cf
James Heck <james.heck1@gmail.com>
2017-11-01 08:33:27 +0000

    account for NULL pass

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,15 +11,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *temp;
 
-	if (*head == NULL)
-	{
-		return (NULL); }
 	temp = *head;
 	new = (listint_t *) malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
 		return (NULL); }
 	new->n = n;
+
+	if (*head == NULL)
+	{
+	new->next = *head;
+	*head = new;
+	return (*head); }
 
 	if (idx == 0)
 	{
