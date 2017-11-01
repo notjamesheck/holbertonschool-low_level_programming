f5793fc5b80954c2d8c53f6975b4c5c3d25667b5
James Heck <james.heck1@gmail.com>
2017-11-01 06:28:13 +0000

    10

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,8 +20,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL); }
 	if (idx == 0)
 	{
-		new->next = temp->next;
-		temp->next = new;
+		new->next = *head;
+		*head = new;
 		return (new); }
 	while (idx > 1)
 	{
