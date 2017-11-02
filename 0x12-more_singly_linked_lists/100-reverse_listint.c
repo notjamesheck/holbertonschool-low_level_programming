c5d4ae7b1c3a94e852224a64ac902f6d4264a7e2
James Heck <james.heck1@gmail.com>
2017-11-02 05:19:03 +0000

    Exercise 100

---
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+* reverse_listint - Entry point
+* @head: pointer to node
+* Return: reversed list
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *before = NULL;
+	listint_t *after = (*head)->next;
+
+	if (*head == NULL)
+	{
+		return (NULL); }
+	while ((*head)->next != NULL)
+	{
+		(*head)->next = before;
+		before = *head;
+		*head = after;
+		after = after->next;
+	}
+	(*head)->next = before;
+	return (*head);
+}
