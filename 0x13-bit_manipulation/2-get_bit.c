53043c931f2fd6a97f42c2b6ee1ea0f4ddada7fd
notjamesheck <224@holbertonschool.com>
2017-11-04 05:39:35 +0000

    2

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* get_bit - Entry point
+* @n: int parameter
+* @index: int parameter
+* Return: 1, 0 or -1 on failure
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int shifted = 0;
+
+	shifted = n >> index;
+	if ((shifted & 1) == 1)
+		return (1);
+	else if ((shifted & 1) == 0)
+		return (0);
+	else
+		return (-1);
+}
--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -157,6 +157,8 @@ unsigned int binary_to_uint(const char *b);
 
 void print_binary(unsigned long int n);
 
+int get_bit(unsigned long int n, unsigned int index);
+
 /* #define NULL ((char *)0) */
 
 #endif /* HOLBERTON_H */
