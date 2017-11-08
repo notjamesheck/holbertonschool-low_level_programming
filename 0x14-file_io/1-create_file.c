fc4de7f6ac97c4f24e21b5b7b1f5c608cc6e2dee
notjamesheck <224@holbertonschool.com>
2017-11-08 07:46:25 +0000

    _

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -23,9 +23,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, count;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1); }
+	if (text_content == NULL)
+	{
+		file = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
+		return (1); }
 
 	file = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
 	if (file == -1)
