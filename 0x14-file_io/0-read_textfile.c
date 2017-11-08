5e0e26ceefdffec730eb5e59eb9a530bbe15588a
notjamesheck <224@holbertonschool.com>
2017-11-08 03:05:31 +0000

    0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,27 +1,39 @@
 #include "holberton.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
+/**
+* read_text - Entry point
+* @filename: pointer to file
+* @letters: size_t parameter
+* Return: chars printed on success, 0 on failure
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, ret = 0;
 
-	char *buf = NULL;	
+	char *buf = NULL;
 
 	buf = malloc((sizeof(char) * letters) + 1);
-	
+	if (buf == NULL)
+	{
+		return (0); }
+
 	fd = open(filename, O_RDONLY);
-	
+	if (fd == -1)
+	{
+		free(buf);
+		return (0); }
+
 	ret = read(fd, buf, letters);
-	
+	if (ret == -1)
+	{
+		free(buf);
+		return (0); }
+
 	write(STDOUT_FILENO, buf, ret);
 
 	close(fd);
 
 	free(buf);
-	
+
 	return (ret);
 }
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -2,10 +2,13 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
-
 #include <string.h>
-
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 int _putchar(char c);
 
