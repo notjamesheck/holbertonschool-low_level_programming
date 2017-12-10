1ba5d0b2593ce43c97ec8ba95802be307b290f3f
notjamesheck <224@holbertonschool.com>
2017-12-10 22:05:45 +0000

    2

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		return (NULL); }
 	new->n = n;
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		new->next = NULL;
 		new->prev = NULL;
@@ -26,6 +26,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (*head); }
 	new->prev = NULL;
 	new->next = *head;
+	(*head)->prev = new;
 	*head = new;
 	return (*head);
 }
