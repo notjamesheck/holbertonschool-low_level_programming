fb7585d9e58f0edbe8bfd567c9c820b8f30a73d5
notjamesheck <224@holbertonschool.com>
2017-12-10 06:40:40 +0000

    0

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* print_dlistint - entry point
+* @h: head node of doubly linked list
+* Return: number of nodes, print contents 
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 1;
+
+	if(h == NULL)
+	{
+		return(0); }
+	while(h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next; }
+	printf("%d\n", h->n);
+	return(count);    
+}
