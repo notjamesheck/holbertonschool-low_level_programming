913628e00e86c291eddef5eec5b7e883d7635cde
notjamesheck <224@holbertonschool.com>
2017-12-10 06:44:10 +0000

    0

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -6,20 +6,20 @@
 /**
 * print_dlistint - entry point
 * @h: head node of doubly linked list
-* Return: number of nodes, print contents 
+* Return: number of nodes, print contents
 */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 1;
 
-	if(h == NULL)
+	if (h == NULL)
 	{
-		return(0); }
-	while(h->next != NULL)
+		return (0); }
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next; }
 	printf("%d\n", h->n);
-	return(count);    
+	return (count);
 }
