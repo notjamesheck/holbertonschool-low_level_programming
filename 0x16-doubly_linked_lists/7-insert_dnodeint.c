773ada6784e070ceb3e90e0120dc77b939b743d4
notjamesheck <224@holbertonschool.com>
2017-12-11 07:40:33 +0000

    f

---
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+* insert_dnodeint_at_index - entry point
+* @h: node pointer
+* @idx: int
+* @n: int
+* Return: something
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new; /* initialize struct type */
+	dlistint_t *temp; /* init struct type */
+
+	temp = *h; /* temporarily save address of head */
+	new  = malloc(sizeof(listint_t));
+
+	if (new == NULL) /* null check malloc */
+	{
+		return (NULL); }
+	new->n = n; /* assign new node value */
+
+	if (*h == NULL) /* if head is null, makes new head*/
+	{
+		new->next = *h;
+		new->prev = *h;
+		*h = new;
+		return (*h); }
+
+	while (idx > 1) /* go to index in list*/
+	{
+		temp = temp->next; /* traverse list */
+		if (temp == NULL) /* if null, index does not exist */
+		{
+			return (NULL); }
+		idx--; }
+	new->next = temp->next; /* make new take place at index */
+	new->prev = temp->prev;
+
+	temp->next = new;
+
+	return (new);
+}
