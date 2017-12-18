61397e13f3eeeb89c8d0cd59d22624e5fce96e4c
notjamesheck <224@holbertonschool.com>
2017-12-18 05:23:18 +0000

    7

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,32 +10,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new; /* initialize struct type */
 	dlistint_t *temp; /* init struct type */
-	int i = 0;
+	unsigned int c;
 
 	temp = *h; /* temporarily save address of head */
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL) /* null check malloc */
-	{
-		return (NULL); }
-	new->n = n; /* assign new node value */
 
 	if (*h == NULL || idx == 0) /* if head is null, makes new head*/
 	{
 		add_dnodeint(h, n); } /* using add_dnode function */
 
-	while (idx > i) /* go to index in list*/
+	for (c = 0; idx > c; c++) /* go to index in list*/
 	{
-		if (temp->next == NULL)
-		{
-			add_dnodeint_end(h, n); }
-		else
-		{
-			temp = temp->next; } /* traverse list */
+		temp = temp->next;  /* traverse list */
 		if (temp == NULL) /* if null, index does not exist */
-		{
-			return (NULL); }
-		i++; }
+			return (NULL);
+	}
+
+	if (temp->next == NULL)
+	{
+		add_dnodeint_end(h, n); }
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL); }
+	new->n = n;
 	new->next = temp->next; /* make new take place at index */
 	new->prev = temp->prev;
 
