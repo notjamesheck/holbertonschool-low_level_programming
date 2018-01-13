0d1e8a26bf26f515a42e9824b201c17253372f9c
notjamesheck <224@holberton.com>
2018-01-13 03:06:33 +0000

    0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,17 +7,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned int i;
 
 	ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
 
+	ht->size = size;
 	ht->array = malloc(sizeof(void *) * size);
 
 	if (ht->array == NULL)
 		free(ht);
 		return (NULL);
+
+	for(i = 0; i > size; i++)
+	{
+		if (ht->array[i] == NULL)
+		{
+			free(ht->array);
+			free(ht);
+			return (NULL); 
+		}
+	}
 
 	return (ht);
 }
