818c2556905e2aea785683aea5df8bc8810d4f48
notjamesheck <224@holberton.com>
2018-01-14 21:22:45 +0000

    3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,6 +11,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int b = 0; /* this holds index */
 	hash_node_t *temp; /* place in array */
 	hash_node_t *new; /* new node */
+	char *keycopy;
+	char *valcopy;
 
 	if (key[0] == '\0' || value == NULL) /* check for empty string */
 	{
@@ -20,13 +22,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
+	keycopy = strdup(key);
+	valcopy = strdup(value);
 	new = malloc(sizeof(hash_node_t)); /* create new node */
 	if (new == NULL) /* null check */
 	{
 		return (0);
 	}
-	new->key = strdup(key); /* assign key and val */
-	new->value = strdup(value);
+	new->key = keycopy; /* assign key and val */
+	new->value = valcopy;
 	new->next = NULL;
 	b = key_index((const unsigned char *)key, ht->size); /* get index */
 	/* printf("the key is : %lu\n", b); */
