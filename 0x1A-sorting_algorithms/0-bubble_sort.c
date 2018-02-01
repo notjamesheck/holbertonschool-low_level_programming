e5560d8f309e06b03e68c23df608fe93dfff7b9f
notjamesheck <224@holberton.com>
2018-02-01 19:14:35 +0000

    O

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -8,22 +8,19 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, temp;
 
-    /* iterate through loop size times */
-	for (i = 0; i < size; i++) 
-	{ 
-	    /* check each elem. and compare with next */
-		for (j = 0; j < size - 1; j++) 
-		{
-			/* if next item in array is larger, swap*/
-            if (array[j] > array[j + 1]) 
+	/* iterate through loop size times */
+	for (i = 0; i < size; i++)
+	{ /* check each elem. and compare with next */
+		for (j = 0; j < size - 1; j++)
+		{ /* if next item in array is larger, swap*/
+			if (array[j] > array[j + 1])
 			{
-				temp = array[j]; 
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = array[j];
-			}
+				array[j + 1] = temp;
+				print_array(array, size); }
 		}
-		print_array(array, size);
 	}
 }
--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,56 +1,39 @@
 #include "sort.h"
 
 
+listint *gotoplc(listint *node)
+{
+	traverse;
+}
+
+void swaploc(listint *node1, listint *node2)
+{
+	swap;
+}
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list; /* head node */
-	listint_t *next; /* next node, after swapping */
-	listint_t *temp; /* traverses list backwards */
-
-	if (!list || !current || !current->next)
+	listint_t *one = *list;
+	listint_t *two = NULL;
+	/* if no list or one element */
+	if (!list || !one || !one->next)
 		return;
-
-	while (current)
+	/*  */
+	while (one)
 	{
-		temp = current; /* temp is previous */
-		current = temp->next; /* move curr up */
-		if (current->next) /* save node for next loop */
+		if (one->n > two->n)
 		{
-			next = current->next; }
-		else
-		{
-			next = NULL; }
-
-		if (temp->n > current->n) /* if sort needed */
-		{ 
-			/* move temp back to place */
-			while (temp->prev && temp->n > current->n)
-			{
-				temp = temp->prev;
-			}
-			if (!temp->prev && temp->n > current->n)
-			{	/* new head node */
-				current->next = temp;
-				temp->prev = current;
-				current->prev = NULL;
-			}
-			else
-			{	/* detach head */
-				current->prev->next = current;
-				current->next->prev = current->prev;
-				/* insert head node */
-				temp->next->prev = current;
-				current->next = temp->next;
-				current->prev = temp;
-				temp->next = current;
-			}
+			re-traverse func;
 			
+			swap func;
+	
+			print;
+			traverse;
 		}
-		if (next) /* if next has an assignment */
-		{
- 			current = next; }
 		else
 		{
-			break; }
+			traverse
+			advance one
+		}
 	}
 }	
