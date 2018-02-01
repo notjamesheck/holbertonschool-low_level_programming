9f2986cd8722e2e98a69bc85bbdb319cfbdf112c
notjamesheck <224@holberton.com>
2018-02-01 08:31:57 +0000

    O

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -7,36 +7,37 @@ void insertion_sort_list(listint_t **list)
 	listint_t *next; /* next node, after swapping */
 	listint_t *temp; /* traverses list backwards */
 
-	if (!list || !current !! !current->next)
+	if (!list || !current || !current->next)
 		return;
 
 	while (current)
 	{
 		temp = current; /* temp is previous */
 		current = temp->next; /* move curr up */
+		if (current->next) /* save node for next loop */
+		{
+			next = current->next; }
+		else
+		{
+			next = NULL; }
+
 		if (temp->n > current->n) /* if sort needed */
 		{ 
-			if (current->next) /* save node for next loop */
-			{
-				next = current->next; }
-			else
-			{
-				next = NULL; }
 			/* move temp back to place */
 			while (temp->prev && temp->n > current->n)
 			{
 				temp = temp->prev;
 			}
-			if (!temp->prev && temp->n > head->n)
+			if (!temp->prev && temp->n > current->n)
 			{	/* new head node */
-				head->next = temp;
+				current->next = temp;
 				temp->prev = current;
 				current->prev = NULL;
 			}
 			else
 			{	/* detach head */
 				current->prev->next = current;
-				current->next->prev = prev;
+				current->next->prev = current->prev;
 				/* insert head node */
 				temp->next->prev = current;
 				current->next = temp->next;
@@ -47,8 +48,9 @@ void insertion_sort_list(listint_t **list)
 		}
 		if (next) /* if next has an assignment */
 		{
-		
-			
-		
-	
-	
+ 			current = next; }
+		else
+		{
+			break; }
+	}
+}	
