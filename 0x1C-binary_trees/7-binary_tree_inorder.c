a05e533ec3650681b3daef134e38f2f5056dc561
notjamesheck <224@holbertonschool.com>
2018-03-09 23:16:28 +0000

    func

--- 0x1C-binary_trees/7-binary_tree_inorder.c
+++ 0x1C-binary_trees/7-binary_tree_inorder.c
@@ -11,7 +11,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree != NULL && func != NULL)
 	{
 		binary_tree_inorder(tree->left, func);
-		print_num(tree->n);
+		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
 }
