4c8980269b58a18ff8288510019b2b9bb385bf50
notjamesheck <224@holbertonschool.com>
2018-03-09 05:38:19 +0000

    1

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -26,7 +26,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left == NULL)
 	{
-		node->parent = parent->left;
+		node->parent = parent;
 		parent->left = node;
 	}
 	else
