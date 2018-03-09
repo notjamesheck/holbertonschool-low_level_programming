56c802c72ba7375b15493005072cb1163ba718a8
notjamesheck <224@holbertonschool.com>
2018-03-09 19:13:06 +0000

    gah!

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -8,10 +8,10 @@
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
-
-	print_num(tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	if (tree != NULL && func != NULL)
+	{
+		print_num(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
