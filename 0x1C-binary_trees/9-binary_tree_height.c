ed11a2f70eeb50aa1f4adb88ee85a91310edec04
notjamesheck <224@holbertonschool.com>
2018-03-09 20:26:34 +0000

    ok

---
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - tree height finder
+* @tree: tree node
+* Return: size_t height
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t x = 0;
+
+	if (tree == NULL)
+	{
+		return (x);
+	}
+
+	while (1)
+	{
+		if (tree->left)
+		{
+			tree = tree->left;
+			x++;
+		}
+		else if (tree->right)
+		{
+			tree = tree->right;
+			x++;
+		}
+		else
+			break;
+	}
+	return (x);
+}
