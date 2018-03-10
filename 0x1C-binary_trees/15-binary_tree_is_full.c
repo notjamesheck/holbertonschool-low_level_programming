a25333c618487c61731c31829d35e634bb335691
notjamesheck <224@holbertonschool.com>
2018-03-10 06:19:46 +0000

    func

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
+/**
+* binary_tree_nodes - counts nodes
+* @tree: tree node
+* Return: # of nodes
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t x = 0;
 
+	if (tree == NULL)
+	{
+		return (x); }
+	if (tree->left || tree->right)
+	{
+		x += 1; }
+	x += binary_tree_nodes(tree->left);
+	x += binary_tree_nodes(tree->right);
+	return (x);
+}
 /**
 * binary_tree_leaves - leaves
 * @tree: tree node
@@ -11,20 +29,14 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree == NULL)
 	{
-		return (x);
-	}
-
+		return (x); }
 	x += binary_tree_leaves(tree->left);
 	x += binary_tree_leaves(tree->right);
-
 	if (!tree->left && !tree->right)
 	{
-		return (x + 1);
-	}
-
+		return (x + 1); }
 	return (x);
 }
-
 /**
 * binary_tree_is_full - balance factor
 * @tree: tree node
@@ -32,20 +44,15 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int x = 0, y = 0;
+	int x = 0, y = 0, a = 0, b = 0;
 
 	if (tree == NULL /*|| (!tree->left && !tree->right)*/)
 	{
-		return (0);
-	}
-
-	/* if (!tree->left || !tree->right) */
-	/* { */
-	/*	return (-1); */
-	/*} */
-
+		return (0); }
 	x = (int)binary_tree_leaves(tree->left);
 	y = (int)binary_tree_leaves(tree->right);
+	a = (int)binary_tree_nodes(tree->left);
+	b = (int)binary_tree_nodes(tree->right);
 	/* printf("x = %d & y = %d\n", x, y); */
-	return (x == y);
+	return ((x == y) && (a == b));
 }
